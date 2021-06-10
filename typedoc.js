c3ad91a837f74978d005a0059b5e8ce24500e2e9
Vladimir Ignatov <vlad.ignatov@gmail.com>
2021-06-09 20:18:22 -0400

    docs

--- typedoc.js
+++ typedoc.js
@@ -12,10 +12,10 @@ module.exports = {
     out: "docs/typedoc",
 
     // Define patterns for excluded files when specifying paths.
-    exclude: ["node_modules"],
+    exclude: ["node_modules", "src/index.ts", "src/lib/abortcontroller-polyfill.d.ts"],
 
     // Define patterns for files that should be considered being external.
-    externalPattern: "node_modules",
+    externalPattern: ["node_modules", "abortcontroller-polyfill"],
 
     // Prevent externally resolved TypeScript files from being documented.
     excludeExternals: true,
@@ -47,23 +47,42 @@ module.exports = {
     // defaultCategory: "fhirclient",
     // --disableOutputCheck      Should TypeDoc disable the testing and cleaning of the output directory?
     // --entryPoint              Specifies the fully qualified name of the root symbol. Defaults to global namespace.
-    // entryPoint: "FHIR",
+    // entryPoint: "fhirclient",
     // --gaID                    Set the Google Analytics tracking ID and activate tracking code.
     // --gaSite                  Set the site name for Google Analytics. Defaults to `auto`.
     // --gitRevision             Use specified revision instead of the last revision for linking to GitHub source files.
     // --hideGenerator           Do not print the TypeDoc link at the end of the page.
-    // --includes DIRECTORY      Specifies the location to look for included documents (use [[include:FILENAME]] in comments).
+    
+    // Specifies the location to look for included documents
+    // (use [[include:FILENAME]] in comments).
+    includes: "docs",
+    
     // --json                    Specifies the location and file name a json file describing the project is written to.
-    // --listInvalidSymbolLinks  Emits a list of broken symbol [[navigation]] links after documentation generation
+    
+    // Emits a list of broken symbol [[navigation]] links after documentation
+    // generation
     listInvalidSymbolLinks: true,
-    // --logger                  Specify the logger that should be used, 'none' or 'console'
+    
+    // Specify the logger that should be used, 'none' or 'console'
+    logger: "console",
+    
     // --media DIRECTORY         Specifies the location with media files that should be copied to the output directory.
     // --options                 Specify a js option file that should be loaded. If not specified TypeDoc will look for 'typedoc.js' in the current directory.
     // --plugin                  Specify the npm plugins that should be loaded. Omit to load all installed plugins, set to 'none' to load no plugins.
-    // --readme                  Path to the readme file that should be displayed on the index page. Pass `none` to disable the index page and start the documentation on the globals page.
+    
+    // Path to the readme file that should be displayed on the index page.
+    // Pass `none` to disable the index page and start the documentation
+    // on the globals page.
     readme: "../README.md",
-    // --theme                   Specify the path to the theme that should be used or 'default' or 'minimal' to use built-in themes.
-    theme: "docs/typedoc-theme"
+    
+    // Specify the path to the theme that should be used or 'default' or
+    // 'minimal' to use built-in themes.
+    theme: "docs/typedoc-theme",
+    
     // --toc                     Specifies the top level table of contents.
-    // --tsconfig                Specify a typescript config file that should be loaded. If not specified TypeDoc will look for 'tsconfig.json' in the current directory.
+
+    // Specify a typescript config file that should be loaded. If not
+    // specified TypeDoc will look for 'tsconfig.json' in the current
+    // directory.
+    tsconfig: "tsconfig.json"
 };
